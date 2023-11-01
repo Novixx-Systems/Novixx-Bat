6a4bbd8f17c2eac4716054efc0f631a75f78cb2a
Goomba <84668888+GoombaProgrammer@users.noreply.github.com>
2023-11-01 15:08:21 +0100

    add labels

--- Novixx Bat/Shell.cs
+++ Novixx Bat/Shell.cs
@@ -1,8 +1,12 @@
-﻿namespace Novixx_Bat
+﻿using System.Diagnostics;
+
+namespace Novixx_Bat
 {
     internal class Shell
     {
         Dictionary<string, string> variables = new Dictionary<string, string>();
+        Dictionary<string, int> labels = new Dictionary<string, int>();
+        string code = "";
         public Shell(string file)
         {
             Execute(File.ReadAllText(file));
@@ -23,6 +27,7 @@
             string[] lines = command.Split('\n');
             foreach (string line in lines)
             {
+                code += line.Trim() + "\n";
                 string cmd = line.Trim().Split(' ')[0].ToLower();
                 string[] args = line.Trim().Split(' ').Skip(1).ToArray();
 
@@ -30,6 +35,117 @@
                 if (cmd == "echo")
                 {
                     Console.WriteLine(ParseVars(string.Join(" ", args)));
+                }
+                else if (cmd.StartsWith(":") && !cmd.StartsWith("::"))
+                {
+                    labels[cmd.Substring(1)] = code.Split('\n').Length - 1;
+                }
+                else if (cmd == "goto")
+                {
+                    if (args.Length == 0)
+                    {
+                        Console.WriteLine("Usage: goto <label>");
+                    }
+                    else
+                    {
+                        if (labels.ContainsKey(args[0]))
+                        {
+                            code = code.Split('\n')[labels[args[0]]] + "\n";
+                        }
+                        else
+                        {
+                            Console.WriteLine("Label not found");
+                        }
+                    }
+                }
+                else if (cmd == "if")
+                {
+                    if (args.Length == 0)
+                    {
+                        Console.WriteLine("Usage: if <condition> <command>");
+                    }
+                    else
+                    {
+                        if (args[0] == "exist")
+                        {
+                            if (args.Length == 1)
+                            {
+                                Console.WriteLine("Usage: if exist <file> <command>");
+                            }
+                            else
+                            {
+                                if (File.Exists(args[1]))
+                                {
+                                    Execute(string.Join(" ", args.Skip(2)));
+                                }
+                            }
+                        }
+                        else if (args[0] == "not")
+                        {
+                            if (args.Length == 1)
+                            {
+                                Console.WriteLine("Usage: if not <condition> <command>");
+                            }
+                            else
+                            {
+                                if (args[1] == "exist")
+                                {
+                                    if (args.Length == 2)
+                                    {
+                                        Console.WriteLine("Usage: if not exist <file> <command>");
+                                    }
+                                    else
+                                    {
+                                        if (!File.Exists(args[2]))
+                                        {
+                                            Execute(string.Join(" ", args.Skip(3)));
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                        else
+                        {
+                            if (args[1] == "equ")
+                            {
+                                if (args[0] == args[2])
+                                {
+                                    // Execute the command
+                                    Execute(string.Join(" ", args.Skip(3)));
+                                }
+                            }
+                        }
+                    }
+                }
+                else if (cmd == "call")
+                {
+                    if (args.Length == 0)
+                    {
+                        Console.WriteLine("Usage: call <file>");
+                    }
+                    else
+                    {
+                        Execute(File.ReadAllText(args[0]));
+                    }
+                }
+                else if (cmd == "pause")
+                {
+                    Console.ReadKey();
+                }
+                else if (cmd == "rem")
+                {
+                    // Do nothing
+                }
+                else if (cmd == "start")
+                {
+                    if (args.Length == 0)
+                    {
+                        Console.WriteLine("Usage: start <file>");
+                    }
+                    else
+                    {
+                        Process.Start(args[0]);
+                    }
                 }
                 else if (cmd == "exit")
                 {
